15278c86d5d1d7951ecd90da19d9b57c253845f4
Saurav <saurav@ayopop.com>
2020-03-21 13:56:38 +0530

    loginANDregister

---
+++ routes/registration.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/register', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+module.exports = router;
