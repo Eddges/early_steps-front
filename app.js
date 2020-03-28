ccc878822d91c0310aa6d142f6b584c29b8cc782
kaushal77 <kaushalmillionare96@gmail.com>
2020-03-28 12:02:51 +0530

    corrections with UI changes
    - logout footer in leaderboard
    - "creative studio" credits footer removed from all pages
    - title and icon changed to codeasylums
    - notification and messages changed
    - country flag removed
    - dashboard default user photo changed

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use('/register', indexRouter);
 app.use('/dashboard', indexRouter);
 app.use('/schedule', indexRouter);
 app.use('/videos', indexRouter);
+app.use('/rewards', indexRouter);
 app.use('/leaderboard', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
